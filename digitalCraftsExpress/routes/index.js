d2ae37a76de52753caf73b655d4d12c1eb3ebaff
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-16 15:12:32 -0500

    edited index.ejs and index.js

--- digitalCraftsExpress/routes/index.js
+++ digitalCraftsExpress/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express', 
+    students: [
+      "Zac",
+      "Jim",
+      "JR",
+      "Matt"
+    ],
+  });
 });
 
 module.exports = router;
